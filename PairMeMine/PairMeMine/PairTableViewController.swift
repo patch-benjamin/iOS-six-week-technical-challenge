b1182e2bf1e8a0967661d85cab91370aad4d92b4
Benjamin Patch <benjaminarthurpatch@gmail.com>
2015-11-18 15:46:50 -0700

    Implemented Delete option
    You can now delete a person on the person list view

--- PairMeMine/PairMeMine/PairTableViewController.swift
+++ PairMeMine/PairMeMine/PairTableViewController.swift
@@ -75,19 +75,20 @@ class PairTableViewController: UITableViewController {
         return true
     }
     */
-
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            
+            PersonController.sharedInstance.removePerson(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
     */
-
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
